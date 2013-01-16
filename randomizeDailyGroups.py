c6deed53a1298533bc4a69a8ff170008b8552bcd
Mark Betnel <itmeson@gmail.com>
2013-01-16 11:36:09 -0500

    made two randomize scripts, to generate class groups DURING class

---
+++ randomizeDailyGroups.py
@@ -0,0 +1,25 @@
+import sys, random
+
+classPATH = sys.argv[1]
+day = int(sys.argv[3]) + 6
+whichCLASS = sys.argv[2]
+
+classFILE = open(classPATH + 'attendance.csv', 'rU')
+
+studentsPRESENT = []
+for (i, line) in enumerate(classFILE):
+    if i == 0:
+        continue
+    data = line.strip().split(',')
+    if data[6] == whichCLASS:
+        if data[day] == '1':
+            studentsPRESENT.append(data[1:3])
+
+random.shuffle(studentsPRESENT)
+group = 0
+for (i,n) in enumerate(studentsPRESENT):
+    if i%4 == 0:
+        group += 1
+	print "GROUP ", group
+    print '\t', i+1, n[0], ',', n[1]
+
